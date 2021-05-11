9dd66ae80d043acf2823de4d5df248fa653d90bc
Orhan Albayati <70097003+Orhan92@users.noreply.github.com>
2021-05-11 11:39:16 +0200

    Updated structure

---
+++ LudoGameApi/Objects/GameSession.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LudoGameApi.Objects
+{
+    public class GameSession
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Player> Players { get; set; }
+    }
+}
---
+++ LudoGameApi/Objects/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LudoGameApi.Objects
+{
+    public class Player
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string PlayerName { get; set; }
+        public Color Color { get; set; }
+        [Required]
+        public ICollection<GamePiece> Pieces { get; set; }
+    }
+}
