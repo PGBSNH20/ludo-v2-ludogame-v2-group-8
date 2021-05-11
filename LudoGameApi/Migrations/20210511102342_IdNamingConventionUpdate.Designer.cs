2d6d6e711935c66d569aa7506f24fc0fea3df16a
Orhan Albayati <70097003+Orhan92@users.noreply.github.com>
2021-05-11 12:27:41 +0200

    Added migration, renamed Id and moved datamodels

--- LudoGameApi/Migrations/20210511102342_IdNamingConventionUpdate.Designer.cs
+++ LudoGameApi/Migrations/20210511102342_IdNamingConventionUpdate.Designer.cs
@@ -4,14 +4,16 @@ using LudoGameApi.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace LudoGameApi.Migrations
 {
     [DbContext(typeof(LudoGameContext))]
-    partial class LudoGameContextModelSnapshot : ModelSnapshot
+    [Migration("20210511102342_IdNamingConventionUpdate")]
+    partial class IdNamingConventionUpdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
