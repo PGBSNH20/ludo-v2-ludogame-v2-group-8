f1a9225428b619cbc08daa46f996147802a3d648
MazdakE <70197891+MazdakE@users.noreply.github.com>
2021-05-20 20:45:29 +0200

    Added some new properties in GamePiece Model.

--- LudoGameApi/Models/GamePiece.cs
+++ LudoGameApi/Models/GamePiece.cs
@@ -6,6 +6,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace LudoGameApi.Models
 {
     public class GamePiece
@@ -14,9 +15,14 @@ namespace LudoGameApi.Models
         [Required]
         public string Name { get; set; }
         public Color Color { get; set; }
-        public int TopPosition { get; set; }
-        public int LeftPosition { get; set; }
-        public bool InGoal { get; set; }
+        public double TopPosition { get; set; }
+        public double LeftPosition { get; set; }
+        [Range(0,44)]
+        public int PositionOnBoard { get; set; }
+        [Range(0,1)]
+        public int OnBoard { get; set; }
+        [Range(0,4)]
+        public int InGoal { get; set; }
         [Required]
         public Player Player { get; set; } // For join database tables.
 
