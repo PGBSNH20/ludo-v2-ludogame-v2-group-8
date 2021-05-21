98f90f8790d653a7261792b3354f657fd6b650d0
Kio <48633146+Kiomars93@users.noreply.github.com>
2021-05-21 15:24:50 +0200

    Super close to get a response from our API
    Almost done with a test version for our New Game Button

--- LudoGameV2/Pages/Ludo.cshtml.cs
+++ LudoGameV2/Pages/Ludo.cshtml.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using RestSharp;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace LudoGameV2.Pages
@@ -17,15 +18,22 @@ namespace LudoGameV2.Pages
         //    _restClient = restClient;
         //    _restClient.BaseUrl = new Uri("https://localhost:44393/api/");
         //}
-        [BindProperty]
-        public string RequestMethod { get; set; }
+        //[BindProperty]
+        //public string RequestMethod { get; set; }
 
+        //[BindProperty]
+        //public string Data { get; set; }
+
+        //[BindProperty]
+        //public string BaseUrl { get; set; }
+        HttpClient client = new HttpClient();
+        //[BindProperty]
+        //public List<GameSession> GameSessions { get; set; }
         [BindProperty]
         public string Data { get; set; }
-
         [BindProperty]
-        public string BaseUrl { get; set; }
-        public async Task<List<GameSession>> OnGetAsync()
+        public string RequestMethod { get; set; }
+        public async Task<IActionResult> OnPostAsync()
         {
             //var request = new RestRequest("GameSessions/", DataFormat.Json);
             //var peopleResponse = await _restClient.ExecuteAsync(request);
@@ -40,26 +48,35 @@ namespace LudoGameV2.Pages
             // 2) anropa API:ET för att hämta info från DB:
             // 3) New game load game knappar behövs till razor page
             // 4) Kunna koppla det till javascript när man hämtar datan
-            var game = new List<GameSession>();
-            var client = new RestClient("https://localhost:44393/api/");
+            string responseContent = "https://localhost:44393/api/GameSessions/";
 
-            var request = new RestRequest("GameSessions/", Method.GET, DataFormat.Json);
-            var response = await client.GetAsync<List<GameSession>>(request);
+            //var client = new RestClient("https://localhost:44393/api/");
+
+            //var request = new RestRequest("GameSessions/", Method.GET, DataFormat.Json);
+            //_request = new RestRequest("GameSessions/", Method.GET, DataFormat.Json);
+            //var response = await client.GetAsync<List<GameSession>>(RequestMethod);
+            HttpResponseMessage response = await client.GetAsync(responseContent.ToString());
+
+
+            if (RequestMethod.Equals("GET"))
+            {
+                responseContent = await response.Content.ReadAsStringAsync();
+            }
             //request.OnBeforeDeserialization = resp => { resp.ContentType = "application/json"; };
 
-            foreach (var g in response)
-            {
-                game.Add(g);
-            }
+            //foreach (var g in response)
+            //{
+            //    game.Add(g);
+            //}
             //var queryResult = await client.ExecuteAsync(request);
-            return game;
+            return RedirectToPage("index", new { result = responseContent });
         }
-        public async Task<IActionResult> OnPost()
-        {
-            string responseContent = "[]";
+        //public async Task<IActionResult> OnPost()
+        //{
+        //    string responseContent = "[]";
 
-            return Content(responseContent);
-        }
+        //    return Content(responseContent);
+        //}
 
     }
 }
