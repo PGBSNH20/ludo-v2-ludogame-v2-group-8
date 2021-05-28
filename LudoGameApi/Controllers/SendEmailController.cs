29ad09171c772d88253a61e116673b3ba5ac2d1e
Kio <48633146+Kiomars93@users.noreply.github.com>
2021-05-28 12:52:39 +0200

    Did a simple SendEmail Frontend
    Connected tje frontend to the API

---
+++ LudoGameApi/Controllers/SendEmailController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LudoGameApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class SendEmailController : ControllerBase
+    {
+        // using SendGrid's C# Library
+        // https://github.com/sendgrid/sendgrid-csharp
+
+
+        [HttpPost]
+        public async Task SendEmail(string fromEmail, string toEmail)
+        {
+            //var apiKey = "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";
+            var apiKey = "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";
+            //Environment.GetEnvironmentVariable("NAME_OF_THE_ENVIRONMENT_VARIABLE_FOR_YOUR_SENDGRID_KEY");
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress(fromEmail);
+            var subject = "Ludo game challenge";
+            var to = new EmailAddress(toEmail);
+            var plainTextContent = "I would like to challenge in ludo game";
+            var htmlContent = "<strong>I would like to challenge in ludo game</strong>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg);
+        }
+    }
+}
--- LudoGameV2/Pages/Ludo/Invite.cshtml.cs
+++ LudoGameV2/Pages/Ludo/Invite.cshtml.cs
@@ -8,30 +8,25 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using RestSharp;
 
-
-
 namespace LudoGameV2.Pages.Ludo
 {
     [Authorize]
-    public class CreateSessionModel : PageModel
+    public class InviteModel : PageModel
     {
-        public string GameBoardSessionMessage { get; set; }
         [BindProperty]
-        public NewGameSession NewSession { get; set; }
-        public IActionResult OnPostAsync(string fromEmail, string toEmail)
+        public Mail Mail { get; set; }
+        
+        public IActionResult OnPostAsync()
         {
             if (!ModelState.IsValid)
             {
                 return Page();
             }
-            var client = new RestClient($"https://localhost:44393/api/SendEmail/SendEmail/?{fromEmail}{toEmail}");
+            var client = new RestClient($"https://localhost:44393/api/SendEmail/SendEmail/?{Mail.FromEmail}{Mail.ToEmail}");
             client.Timeout = -1;
-            var request = new RestRequest(Method.GET);
+            var request = new RestRequest(Method.POST);
             request.AddHeader("ApiKey", "secret1234");
-            request.AddJsonBody(NewSession);
             IRestResponse response = client.Execute(request);
-
-            GameBoardSessionMessage = response.Content;
 
             if (response.StatusCode.ToString() == "OK")
             {
